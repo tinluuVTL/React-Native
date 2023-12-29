6a3f7f4919b4fbe8da44413091fe4103df8e78eb
Tín Lưu <107022820+tinluuVTL@users.noreply.github.com>
2023-12-29 12:26:06 +0700

    Add files via upload

---
+++ routing/routing/components/DetailsScreen.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const DetailsScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Details Screen</Text>
+      <Button
+        title="Go back"
+        onPress={() => navigation.goBack()}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+  },
+});
+
+export default DetailsScreen;
---
+++ routing/routing/components/HomeScreen.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+  },
+});
+
+export default HomeScreen;
